02e96363846813293e55b4b28f511f210cb44a70
Brent Washington <brentwashing@gmail.com>
2021-09-03 00:35:20 -0500

    Add two more tests

--- code/practice.js
+++ code/practice.js
@@ -1,14 +1,50 @@
-// Return the sum of two numbers
+// Returns the sum of two numbers
 function sum(a, b) {
     return a + b;
 }
 
-// Capitalize the first letter of a word
+// Capitalizes the first letter of a word
 function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+// Reverses a string 
+function reverseString(str) {
+    const original = [];
+    const reversed = [];
+
+    for (let i = 0; i < str.length; i++) {
+        original.push(str.charAt(i));
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        reversed.push(original.pop());
+    }
+
+    return reversed.join("");
+}
+
+const calculator = {
+    add: (a, b) => {
+        return a + b;
+    },
+
+    subtract: (a, b) => {
+        return a - b;
+    },
+
+    multiply: (a, b) => {
+        return a * b;
+    },
+
+    divide: (a, b) => {
+        return a / b;
+    }
+}
+
 export {
     sum,
-    capitalize
+    capitalize,
+    reverseString,
+    calculator
 } 
--- tests/practice.test.js
+++ tests/practice.test.js
@@ -1,11 +1,23 @@
 import { sum } from '../code/practice';
 import { capitalize } from '../code/practice';
+import { reverseString } from '../code/practice';
+import { calculator } from '../code/practice';
 
 test('adds 1 + 2 to equals 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-
-test('return a string with the first letter capitalized', () => {
+test('returns a string with the first letter capitalized', () => {
     expect(capitalize('foobar')).toBe('Foobar');
+});
+
+test('takes a string and returns it reversed', () => {
+    expect(reverseString('foobar')).toBe('raboof');
+});
+
+test('calculator object that contains four basic operations', () => {
+    expect(calculator.add(1, 2)).toBe(3);
+    expect(calculator.subtract(2, 1)).toBe(1);
+    expect(calculator.multiply(2, 2)).toBe(4);
+    expect(calculator.divide(4, 2)).toBe(2);
 });
